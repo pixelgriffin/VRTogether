f7b579bcde07981a3ebcd247f920ad198893ee5e
Alex Kastanek <akastanek@nevada.unr.edu>
2018-11-10 13:30:26 -0800

    slight modifications to gyro controls

--- VRTogetherAndroid/Assets/Scripts/GyroscopeController.cs
+++ VRTogetherAndroid/Assets/Scripts/GyroscopeController.cs
@@ -7,13 +7,14 @@ public class GyroscopeController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         Input.gyro.enabled = true;
+        transform.localRotation = Quaternion.Euler(90f, 90f, 0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
         Quaternion orientation = Input.gyro.attitude;
-        transform.rotation = new Quaternion(
+        transform.localRotation = new Quaternion(
             orientation.x,
             orientation.y,
             -orientation.z,
