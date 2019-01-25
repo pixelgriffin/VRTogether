82491cb358940b9134a02bb25724d1af64f5b301
Bryce Monaco <Bryce@BrycBook-Pro.local>
2019-01-24 22:39:44 -0800

    Added options submenu

--- VRTogetherAndroid/Assets/Scripts/LobbyManager.cs
+++ VRTogetherAndroid/Assets/Scripts/LobbyManager.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class LobbyManager : MonoBehaviour {
 
 	public string code = ""; //The code or IP to connect with
+	public string username;
 
 	public void SetCode (string newCode)
 	{
 		code = newCode;
+
+	}
+
+	public void SetUsername (string newUsername)
+	{
+		username = newUsername;
 
 	}
 }
