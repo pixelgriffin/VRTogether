ed2ba04997f580ba524139fc7c4a95ebbe9dfe18
Bryce Monaco <Bryce@BrycBook-Pro.local>
2019-01-24 22:31:24 -0800

    Code/IP entered in menu is stored

---
+++ VRTogetherAndroid/Assets/Scripts/LobbyManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyManager : MonoBehaviour {
+
+	public string code = ""; //The code or IP to connect with
+
+	public void SetCode (string newCode)
+	{
+		code = newCode;
+
+	}
+}
