fc4c54b2949756072d15f716300e02c86875ef56
Bryce Monaco <Bryce@BrycBook-Pro.local>
2019-01-25 14:56:17 -0800

    Lobby panel can be reached with room code "DEMO" for demo purposes

--- VRTogetherAndroid/Assets/Scripts/LobbyManager.cs
+++ VRTogetherAndroid/Assets/Scripts/LobbyManager.cs
@@ -1,21 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LobbyManager : MonoBehaviour {
 
-	public string code = ""; //The code or IP to connect with
+	public string code = string.Empty; //The code or IP to connect with
 	public string username;
+
+	public GameObject codePanel;
+	public GameObject ipPanel;
+	public GameObject lobbyPanel;
+
+	public void JoinLobby ()
+	{
+		if (code != string.Empty)
+		{
+			//A test code used to bring up the lobby screen for demo purposes
+			if (code == "DEMO")
+			{
+				ipPanel.SetActive(false);
+				codePanel.SetActive(false);
+				lobbyPanel.SetActive(true);
+
+			}
+
+			//Actual code to connect here...
+
+		}
+
+	}
 
 	public void SetCode (string newCode)
 	{
-		code = newCode;
+		code = newCode.ToUpper();
 
 	}
 
 	public void SetUsername (string newUsername)
 	{
-		username = newUsername;
+		username = newUsername.ToUpper();
 
 	}
 }
