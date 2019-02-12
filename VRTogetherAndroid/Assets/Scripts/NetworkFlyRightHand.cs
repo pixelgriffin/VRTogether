a807cbcb5a924a92a8984d7c5a0d7fdf61bd063e
Alex Kastanek <akastanek@nevada.unr.edu>
2019-02-12 11:19:27 -0800

    VR player is now networked

---
+++ VRTogetherAndroid/Assets/Scripts/NetworkFlyRightHand.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkFlyRightHand : MonoBehaviour {
+
+    private GameObject body;
+    private bool addComponent = true;
+
+	// Use this for initialization
+	void Start () {
+
+        body = GameObject.Find("PlayerBody");
+
+        GameObject body_ik = body.transform.GetChild(0).gameObject;
+        body_ik.GetComponent<IK>().rightHandObj = this.transform;
+        addComponent = false;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+    
+
+    }
+}
