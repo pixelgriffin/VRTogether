a105fe40ef57c94ae0c0a55beff1b2350558a8c2
Alex Kastanek <akastanek@nevada.unr.edu>
2019-03-01 14:36:01 -0800

    Added game over logic

---
+++ VRTogetherDesktop/Assets/Scripts/ChickenShooterScripts/LevelManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTogether.Net;
+
+public class LevelManager : MonoBehaviour {
+
+    private int playersAliveCount;
+
+	// Use this for initialization
+	void Start () {
+
+        playersAliveCount = MacrogameServer.Instance.GetMacroPlayers().Count;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (playersAliveCount <= 0)
+        {
+            Debug.Log("GAME OVER");
+            // return to macro scene
+        }
+
+    }
+
+    public void DecrPlayersAliveCount()
+    {
+        playersAliveCount--;
+    }
+}
