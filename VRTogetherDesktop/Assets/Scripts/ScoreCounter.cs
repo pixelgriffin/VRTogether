730c9cfc55639cf82f9da3101d8ed41974b1224d
Alex Kastanek <akastanek@nevada.unr.edu>
2019-03-05 18:44:07 -0800

    Score tracked on desktop ui

--- VRTogetherDesktop/Assets/Scripts/ScoreCounter.cs
+++ VRTogetherDesktop/Assets/Scripts/ScoreCounter.cs
@@ -1,22 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using VRTogether.Net;
 
-public class ScoreCounter : MonoBehaviour {
-
+public class ScoreCounter : MonoBehaviour
+{
     private NetworkID id;
 
     public int forwardScore;
+    public int goalScore = 8;
     public NetworkInt flyScore = new NetworkInt("flyScore", 0);
 
-	void Start () {
+    public string scoreText = "Grapes Stolen: ";
+    public Text scoreTextObject;
+
+    void Start()
+    {
         id = GetComponent<NetworkID>();
         MinigameServer.Instance.RegisterVariable(id.netID, flyScore);
-	}
+
+        scoreTextObject.text = scoreText + "0/" + goalScore;
+    }
 
     private void Update()
     {
         forwardScore = flyScore.value;
+        scoreTextObject.text = scoreText + forwardScore + "/" + goalScore;
     }
 }
