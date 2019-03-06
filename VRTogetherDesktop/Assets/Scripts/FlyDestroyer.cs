8a40a5838deff1e9985dca61e53ea60ff9432f48
Alex Kastanek <akastanek@nevada.unr.edu>
2019-03-05 17:17:11 -0800

    Added logic to swat fly

---
+++ VRTogetherDesktop/Assets/Scripts/FlyDestroyer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTogether.Net;
+
+public class FlyDestroyer : MonoBehaviour {
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Fly")
+        {
+            MinigameServer.Instance.NetworkDestroy(collision.gameObject);
+        }
+    }
+}
