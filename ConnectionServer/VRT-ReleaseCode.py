ebb4708050092e6cf5fd4f2e64d1ac9ef2fbba24
Ripostory <ripostory@gmail.com>
2019-03-06 14:02:59 -0800

    Added AWS code to Repository Fixes #28

---
+++ ConnectionServer/VRT-ReleaseCode.py
@@ -0,0 +1,49 @@
+
+import boto3
+import json
+import random
+import datetime
+
+print('Loading function')
+dynamo = boto3.client('dynamodb')
+
+def respond(code, message):
+    return {
+        'statusCode': code,
+        'body': message,
+        'headers': {
+            'Content-Type': 'application/json',
+        },
+    }
+    
+def releaseCode(event):
+    code = event["queryStringParameters"]['code']
+    
+    #remove from database
+    dynamo.delete_item(
+        TableName='VRT-IP-table',
+        Key={
+            'code': {
+                'S': code,
+            },
+        }
+    )
+
+def assertQuery(event):
+    #check if required queries exist
+    try:
+        data = event["queryStringParameters"]['code']
+    except:
+        return False
+    return True
+
+
+def lambda_handler(event, context):
+    #check if required queries exist
+    if not assertQuery(event):
+        return respond(400, "A Code was not given.")
+    
+    #inserts a new entry when given an item
+    releaseCode(event)
+
+    return respond(200, "")
