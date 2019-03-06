f6069518d8a9c8ec75126755861aa16785bc4af3
Bryce Monaco <brycedmonaco@gmail.com>
2019-03-05 16:48:49 -0800

    Cups spawn in random positions

---
+++ VRTogetherAndroid/Assets/Scripts/CupHunt/CupSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTogether.Net;
+
+public class CupSpawner : MonoBehaviour
+{
+    public GameObject obj;
+
+    public Transform[] spawnPoints;
+
+    private bool doSpawn = true;
+
+    void Start()
+    {
+
+
+    }
+
+    void Update()
+    {
+        if (doSpawn)
+        {
+            if (MinigameClient.Instance.AllPlayersReady())
+            {
+                GameObject inst = MinigameClient.Instance.NetworkInstantiate(obj);
+                inst.transform.position = spawnPoints[Random.Range(0, 9)].position;
+                doSpawn = false;
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+    public bool HasObjectSpawned()
+    {
+        return !doSpawn;
+    }
+}
