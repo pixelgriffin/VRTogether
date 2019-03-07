8cc72f6c0116da27fd97ef5fe0c7e8f769dda1a2
Ripostory <ripostory@gmail.com>
2019-03-06 16:17:10 -0800

    Added UI raycasting, fixed NetFly grape and huge collider

---
+++ VRTogetherDesktop/Assets/Scripts/UI/HideIfNoUIExists.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class HideIfNoUIExists : MonoBehaviour {
+
+    private MeshRenderer mr;
+
+	void Start () {
+        mr = GetComponent<MeshRenderer>();
+	}
+
+	void Update () {
+        mr.enabled = (GameObject.FindGameObjectsWithTag("UI").Length > 0);
+    }
+}
