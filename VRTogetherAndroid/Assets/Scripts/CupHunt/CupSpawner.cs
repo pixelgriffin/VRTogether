30c950fb108dcba226e7f326955487b7693591d4
Bryce Monaco <brycedmonaco@gmail.com>
2019-03-06 17:07:21 -0800

    Continued work on spawning cups

--- VRTogetherAndroid/Assets/Scripts/CupHunt/CupSpawner.cs
+++ VRTogetherAndroid/Assets/Scripts/CupHunt/CupSpawner.cs
@@ -5,34 +5,58 @@ using VRTogether.Net;
 
 public class CupSpawner : MonoBehaviour
 {
+    private NetworkString spawnString = new NetworkString("spawnString", string.Empty);
+
     public GameObject obj;
 
     public Transform[] spawnPoints;
 
-    private bool doSpawn = true;
+    public bool processedSpawn = false;
+    private NetworkID id;
 
     void Start()
     {
+        id = GetComponent<NetworkID>();
 
+        MinigameClient.Instance.RegisterVariable(id.netID, spawnString);
 
     }
 
     void Update()
     {
-        if (doSpawn)
+        if (!processedSpawn && spawnString.value != string.Empty && MinigameClient.Instance.AllPlayersReady())
         {
-            if (MinigameClient.Instance.AllPlayersReady())
-            {
-                GameObject inst = MinigameClient.Instance.NetworkInstantiate(obj);
-                inst.transform.position = spawnPoints[Random.Range(0, 9)].position;
-                doSpawn = false;
-                Destroy(this.gameObject);
-            }
-        }
-    }
+            Debug.Log("Recieved String: " + spawnString.value);
 
-    public bool HasObjectSpawned()
-    {
-        return !doSpawn;
+            string[] posTuples = spawnString.value.Split('%');  // Splits into "<name>$<index>"
+            int spawnIndex = -1;
+
+            foreach (string tup in posTuples)
+            {
+                if (tup.Split('$')[0] == MinigameClient.Instance.name)
+                {
+                    spawnIndex = int.Parse(tup.Split('$')[1]);
+
+                    break;
+
+                }
+            }
+
+            if (spawnIndex == -1)
+            {
+                Debug.LogError("Could not find index in spawn string: " + spawnString.value);
+
+            }
+
+
+            GameObject inst = MinigameClient.Instance.NetworkInstantiate(obj);
+            inst.transform.position = spawnPoints[spawnIndex].position;
+            processedSpawn = true;
+            Destroy(this.gameObject);
+        } else
+        {
+            Debug.Log(processedSpawn.ToString() + "---" + spawnString.value + "---" + MinigameClient.Instance.AllPlayersReady().ToString());
+
+        }
     }
 }
--- VRTogetherDesktop/Assets/Scripts/CupHunt/CupSpawner.cs
+++ VRTogetherDesktop/Assets/Scripts/CupHunt/CupSpawner.cs
@@ -2,15 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using VRTogether.Net;
+
 public class CupSpawner : MonoBehaviour {
 
+    public NetworkString spawnString = new NetworkString("spawnString", string.Empty);
+    private NetworkID id;
+    private bool processedSpawn = false;
+
 	// Use this for initialization
-	void Start () {
+	void Start () 
+    {
+        id = GetComponent<NetworkID>();
+
+        MinigameServer.Instance.RegisterVariable (id.netID, spawnString);
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update () 
+    {
+		if (!processedSpawn && spawnString.value == string.Empty && MinigameServer.Instance.AllPlayersReady())
+        {
+            int spawnIndex = 0;
+
+            foreach (MacrogamePlayer player in MacrogameServer.Instance.GetMacroPlayers())
+            {
+                spawnString.value += player.name + "$" + spawnIndex + "%";
+
+                spawnIndex++;
+
+            }
+
+            Debug.Log("Sent String: " + spawnString.value);
+
+            MinigameServer.Instance.SendStringToAll (spawnString);
+
+            processedSpawn = true;
+
+        }
+
+    }
 }
