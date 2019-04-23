9146249ec2a58240ce1e8d45601b47a094f53584
Bryce Monaco <brycedmonaco@gmail.com>
2019-04-22 23:10:03 -0700

    Cup can be deleted on the desktop side

--- VRTogetherDesktop/Assets/Scripts/CupHunt/Cup.cs
+++ VRTogetherDesktop/Assets/Scripts/CupHunt/Cup.cs
@@ -2,6 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using VRTogether.Net;
+
 public class Cup : MonoBehaviour {
-    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ball"))
+        {
+            MinigameServer.Instance.NetworkDestroy(transform.parent.gameObject);
+
+        }
+
+    }
+
 }
