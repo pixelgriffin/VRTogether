cb0e25286e0d6032beff5ec2950a00437f907c89
Alex Kastanek <akastanek@nevada.unr.edu>
2019-04-24 02:31:41 -0700

    Added functionality to play bullet sound effects on mobile

---
+++ VRTogetherAndroid/Assets/Scripts/ChickenShooterScripts/ChickenFeed.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickenFeed : MonoBehaviour {
+
+    private AudioSource shotFiredSound, wallCollisionSound;
+
+	// Use this for initialization
+	void Start () {
+
+        AudioSource[] sources = GetComponents<AudioSource>();
+
+        shotFiredSound = sources[0];
+        wallCollisionSound = sources[0];
+
+        shotFiredSound.Play();
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == 12)
+        {
+            wallCollisionSound.Play();
+        }
+    }
+}
