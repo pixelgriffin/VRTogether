1d0f956447669bb5d98bf5b7a865fc9a89a4e9cf
Alex Kastanek <akastanek@nevada.unr.edu>
2019-04-26 14:28:28 -0700

    Slave now plays sound effect when it slides on mobile. Should fix #84

---
+++ VRTogetherAndroid/Assets/Scripts/CupHunt/CupSlaveSlide.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTogether.Net;
+
+public class CupSlaveSlide : MonoBehaviour {
+
+    private GameObject sounds;
+
+    private Vector3 lastPosition;
+
+    private float slideTimer;
+    private float slideInterval;
+
+    private NetworkID id;
+
+	void Start () {
+
+        id = GetComponent<NetworkID>();
+
+        if (MinigameClient.Instance.networkedPrefabs.IsSlave(id.netID))
+        {
+            sounds = GameObject.Find("CupHuntSounds");
+            if (sounds == null)
+                Debug.Log("SOUNDS IS NULL");
+            else Debug.Log("SOUNDS IS OK");
+
+            lastPosition = transform.position;
+
+            slideInterval = 1.0f;
+            slideTimer = 0.0f;
+        }
+		
+	}
+	
+	void Update () {
+
+        if (MinigameClient.Instance.networkedPrefabs.IsSlave(id.netID))
+        {
+            if (transform.position != lastPosition && slideTimer >= slideInterval)
+            {
+                // reset timer
+                slideTimer = 0.0f;
+
+                // play slide sound
+                GameObject soundObject = Instantiate(sounds, Vector3.zero, Quaternion.identity);
+                soundObject.GetComponent<Sounds>().playCupSlide();
+                Destroy(soundObject, 5);
+            }
+
+            lastPosition = transform.position;
+            slideTimer += Time.deltaTime;
+        }
+		
+	}
+}
