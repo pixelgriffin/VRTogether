b258f056950907edb76936d5fd3388c6d071f41f
Alex Kastanek <akastanek@nevada.unr.edu>
2019-04-28 23:50:49 -0700

    Added ready up feature to fly swatter

---
+++ VRTogetherAndroid/Assets/Scripts/Ready.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTogether.Net;
+
+public class Ready : MonoBehaviour {
+
+    private void Update()
+    {
+        if (MinigameClient.Instance.AllPlayersReady())
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
