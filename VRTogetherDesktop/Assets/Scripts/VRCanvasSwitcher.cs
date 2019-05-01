8512cba14b627d4a3cc301714ad30129451c7a28
Alex Kastanek <akastanek@nevada.unr.edu>
2019-05-01 16:43:16 -0700

    Started options canvas

---
+++ VRTogetherDesktop/Assets/Scripts/VRCanvasSwitcher.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRCanvasSwitcher : MonoBehaviour {
+
+	public IEnumerator SwitchCanvas(float time)
+    {
+        Quaternion startRot = transform.rotation;
+        float elapsedTiem = 0f;
+
+        while (elsapsedTime < time)
+        {
+
+        }
+    }
+}
