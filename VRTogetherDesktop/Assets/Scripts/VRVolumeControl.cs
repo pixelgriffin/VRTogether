2a4368657fc98110f173b2bb4bbcde61ae55c728
Alex Kastanek <akastanek@nevada.unr.edu>
2019-05-01 18:52:42 -0700

    main menu now plays sounds

--- VRTogetherDesktop/Assets/Scripts/VRVolumeControl.cs
+++ VRTogetherDesktop/Assets/Scripts/VRVolumeControl.cs
@@ -6,14 +6,22 @@ using UnityEngine.UI;
 
 public class VRVolumeControl : MonoBehaviour {
 
+    public AudioSource playSound;
     public AudioMixerGroup masterMixer, musicMixer, soundMixer;
     public Slider masterSlider, musicSlider, soundSlider;
+    public float playSoundInterval = 0.5f;
+    private float playSoundTimer = 0.0f;
 
-    private void Update()
+    private void Start()
     {
         masterSlider.onValueChanged.AddListener(delegate { OnMasterVolumeChanged(masterSlider.value); });
         musicSlider.onValueChanged.AddListener(delegate { OnMusicVolumeChanged(musicSlider.value); });
         soundSlider.onValueChanged.AddListener(delegate { OnSoundEffectVolumeChanged(soundSlider.value); });
+    }
+
+    private void Update()
+    {
+        playSoundTimer += Time.deltaTime;
     }
 
     public void OnMasterVolumeChanged(float newVolume)
@@ -29,6 +37,11 @@ public class VRVolumeControl : MonoBehaviour {
     public void OnSoundEffectVolumeChanged(float newVolume)
     {
         soundMixer.audioMixer.SetFloat("MasterSound", newVolume);
+        if (playSoundTimer >= playSoundInterval)
+        {
+            playSound.Play();
+            playSoundTimer = 0.0f;
+        }
     }
 
 }
