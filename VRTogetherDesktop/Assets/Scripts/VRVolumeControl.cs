7446db10670a14b447786e65a0b8fef3261cf0d0
Alex Kastanek <akastanek@nevada.unr.edu>
2019-05-01 18:45:07 -0700

    Sliders now adjust volume

--- VRTogetherDesktop/Assets/Scripts/VRVolumeControl.cs
+++ VRTogetherDesktop/Assets/Scripts/VRVolumeControl.cs
@@ -2,19 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class VRVolumeControl : MonoBehaviour {
 
-    public AudioMixerGroup masterMixer;
+    public AudioMixerGroup masterMixer, musicMixer, soundMixer;
+    public Slider masterSlider, musicSlider, soundSlider;
 
-    public void OnMusicVolumeChanged(float newVolume)
+    private void Update()
+    {
+        masterSlider.onValueChanged.AddListener(delegate { OnMasterVolumeChanged(masterSlider.value); });
+        musicSlider.onValueChanged.AddListener(delegate { OnMusicVolumeChanged(musicSlider.value); });
+        soundSlider.onValueChanged.AddListener(delegate { OnSoundEffectVolumeChanged(soundSlider.value); });
+    }
+
+    public void OnMasterVolumeChanged(float newVolume)
     {
         masterMixer.audioMixer.SetFloat("Master", newVolume);
     }
 
+    public void OnMusicVolumeChanged(float newVolume)
+    {
+        musicMixer.audioMixer.SetFloat("MasterMusic", newVolume);
+    }
+
     public void OnSoundEffectVolumeChanged(float newVolume)
     {
-        masterMixer.audioMixer.SetFloat("MasterSound", newVolume);
+        soundMixer.audioMixer.SetFloat("MasterSound", newVolume);
     }
 
 }
